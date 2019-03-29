e016acd76b0740839c9827c6afa85060892eb34b
wangmingzhi <wangmz199347@sina.com>
2019-03-29 17:43:38 +0800

    完善代码-service

---
+++ app/src/main/java/com/pangmao/learnbase/aidl/Book.java
@@ -0,0 +1,73 @@
+package com.pangmao.learnbase.aidl;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Book implements Parcelable {
+    private int price;
+    private String bookName;
+
+    private Book(Parcel in) {
+        this.price = in.readInt();
+        this.bookName = in.readString();
+    }
+
+    public Book() {
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.price);
+        dest.writeString(this.bookName);
+    }
+
+    /**
+     * 参数是一个Parcel,用它来存储与传输数据
+     */
+    public void readFromParcel(Parcel dest) {
+        //注意，此处的读值顺序应当是和writeToParcel()方法中一致的
+        price = dest.readInt();
+        bookName = dest.readString();
+    }
+
+    public static final Parcelable.Creator<Book> CREATOR = new Parcelable.Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel source) {
+            return new Book(source);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "price=" + price +
+                ", bookName='" + bookName + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/pangmao/learnbase/service/AIDLService.java
@@ -0,0 +1,72 @@
+package com.pangmao.learnbase.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+
+import com.pangmao.learnbase.aidl.Book;
+import com.pangmao.learnbase.aidl.BookManager;
+import com.pangmao.learnbase.util.LogUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AIDLService extends Service {
+    public AIDLService() {
+    }
+
+    //包含Book对象的list
+    private List<Book> mBooks = new ArrayList<>();
+
+    //由AIDL文件生成的BookManager
+    private final BookManager.Stub mBookManager = new BookManager.Stub() {
+        @Override
+        public List<Book> getBooks() throws RemoteException {
+            synchronized (this) {
+                LogUtil.log("invoking getBooks() method , now the list is : " + mBooks.toString());
+                if (mBooks != null) {
+                    return mBooks;
+                }
+                return new ArrayList<>(10);
+            }
+        }
+
+
+        @Override
+        public void addBook(Book book) throws RemoteException {
+            synchronized (this) {
+                if (mBooks == null) {
+                    mBooks = new ArrayList<>();
+                }
+                if (book == null) {
+                    LogUtil.log("Book is null in In");
+                    book = new Book();
+                }
+                //尝试修改book的参数，主要是为了观察其到客户端的反馈
+                book.setPrice(2333);
+                if (!mBooks.contains(book)) {
+                    mBooks.add(book);
+                }
+                //打印mBooks列表，观察客户端传过来的值
+                LogUtil.log("invoking addBooks() method , now the list is : " + mBooks.toString());
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Book book = new Book();
+        book.setBookName("Android开发艺术探索");
+        book.setPrice(28);
+        mBooks.add(book);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        LogUtil.log(String.format("on bind,intent = %s", intent.toString()));
+        return mBookManager;
+    }
+
+}
